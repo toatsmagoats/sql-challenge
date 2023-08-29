f0a8e69e9bfed55b876f8f40a730ca446f4b6c92
toatsmagoats <imalexfok@gmail.com>
2023-08-28 18:20:59 -0700

    SQL schema and queries

---
+++ EmployeeSQL/employee_schema.sql
@@ -0,0 +1,60 @@
+CREATE TABLE "Departments" (
+    "dept_no" varchar(255)   NOT NULL,
+    "dept_name" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_Emp" (
+    "emp_no" varchar(255)   NOT NULL,
+    "dept_no" varchar(255)   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" varchar(255)   NOT NULL,
+    "emp_no" varchar(255)   NOT NULL
+);
+
+CREATE TABLE "Employees" (
+    "emp_no" varchar(255)   NOT NULL,
+    "emp_title_id" varchar(255)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "sex" varchar(255)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" varchar(10)   NOT NULL,
+    "salary" int   NOT NULL
+);
+
+CREATE TABLE "Titles" (
+    "title_id" varchar(255)   NOT NULL,
+    "title" varchar(255)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Salaries" ALTER COLUMN "emp_no" TYPE varchar(255);
